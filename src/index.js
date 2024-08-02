347d95477533c0288954baef68836f4ecd368139
yelantsevv <yelantsevv@gmail.com>
2024-08-02 13:54:29 +0300

    fix: npm test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (typeof matrix !== "object") return [];
+    return matrix.map((arr, i) => (i % 2 === 0 ? arr : arr.reverse())).flat();
+};
